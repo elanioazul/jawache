d22b77a3e34808981f2c84000d207c2d8ccb2123
elanioazul_hugo <hugo.herrador@gmail.com>
2021-05-25 13:31:09 +0200

    Demostrates hooks for the Component’s Children and initialice input prop. KEYYYYYYYY

--- apps/jawache-tutorial/src/app/components/joke-list/joke-list.component.ts
+++ apps/jawache-tutorial/src/app/components/joke-list/joke-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Joke } from '../../classes/joke.class';
 
 @Component({
@@ -6,30 +6,18 @@ import { Joke } from '../../classes/joke.class';
   templateUrl: './joke-list.component.html',
   styleUrls: ['./joke-list.component.scss']
 })
-export class JokeListComponent implements OnInit {
+export class JokeListComponent  {
 
-  public jokes: Joke[];
+  public jokes: Joke[] = [];
 
-  constructor() { 
-    this.jokes = [
-      new Joke("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
-      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
-      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
-    ];
+  constructor() {}
+
+  addJoke() {
+    this.jokes.unshift(new Joke("What did the cheese say when it looked in the mirror", "Hello-me (Halloumi)"));
   }
 
-  addJoke(joke: Joke) {
-    this.jokes.unshift(joke);
-  }
-
-  deleteJoke(joke: Joke) {
-    const indexToDelete = this.jokes.indexOf(joke);
-    if (indexToDelete !== -1) {
-      this.jokes.splice(indexToDelete,1);
-    }
-  }
-
-  ngOnInit(): void {
+  deleteJoke() {
+    this.jokes = [];
   }
 
 }
--- apps/jawache-tutorial/src/app/components/joke/joke.component.ts
+++ apps/jawache-tutorial/src/app/components/joke/joke.component.ts
@@ -1,4 +1,18 @@
-import { Component, Input, Output, EventEmitter, OnInit, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  Input, 
+  Output, 
+  EventEmitter, 
+  ViewEncapsulation,
+  OnInit,  
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 import { Joke } from '../../classes/joke.class';
 @Component({
   selector: 'jawache-tutorial-joke',
@@ -6,17 +20,56 @@ import { Joke } from '../../classes/joke.class';
   styleUrls: ['./joke.component.scss'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class JokeComponent implements OnInit {
+export class JokeComponent implements
+  OnChanges,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
 
   @Input('joke') data!: Joke;
   @Output() jokeDeleted = new EventEmitter<Joke>();
 
 
-  constructor() { }
+  constructor() { 
+    console.log(`new - data is ${this.data}`)
+  }
+
+  ngOnChanges() {
+    console.log(`ngOnChanges - data is ${this.data}`);
+  }
 
   ngOnInit(): void {
+    console.log(`ngOnInit  - data is ${this.data}`);
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck")
   }
   
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
+
   deleteItem() {
     this.jokeDeleted.emit(this.data);
   }
