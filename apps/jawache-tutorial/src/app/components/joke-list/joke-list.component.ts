81037922f7f4ef72a70565443a7cf70e2d9e50a3
elanioazul_hugo <hugo.herrador@gmail.com>
2021-05-26 13:34:06 +0200

    Add ViewChild Referencing a Template Local Variable and gets modified by accessing DOM node

--- apps/jawache-tutorial/src/app/components/joke-list/joke-list.component.ts
+++ apps/jawache-tutorial/src/app/components/joke-list/joke-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ViewChildren, QueryList, ElementRef, ViewContainerRef, ContentChild, AfterViewInit, AfterContentInit } from '@angular/core';
+import { Component, ViewChild, ViewChildren, QueryList, ViewContainerRef, AfterViewInit, ElementRef } from '@angular/core';
 import { Joke } from '../../classes/joke.class';
 import { JokeComponent } from '../joke/joke.component'
 
@@ -7,11 +7,11 @@ import { JokeComponent } from '../joke/joke.component'
   templateUrl: './joke-list.component.html',
   styleUrls: ['./joke-list.component.scss']
 })
-export class JokeListComponent implements AfterViewInit, AfterContentInit {
+export class JokeListComponent implements AfterViewInit {
 
   @ViewChild(JokeComponent) jokeViewChild: JokeComponent | undefined;
-  //@ViewChildren(JokeComponent, { read: ViewContainerRef }) jokeViewChildren: QueryList<JokeComponent> | undefined
-  @ContentChild(JokeComponent)('insideNgContent') insideNgContent: any;
+  @ViewChildren(JokeComponent, { read: ViewContainerRef }) jokeViewChildren: QueryList<JokeComponent> | undefined
+  @ViewChild('header') headerEl: ElementRef | undefined;
   public jokes: Joke[] = [
     new Joke("What did the cheese say when it looked in the mirror", "Hello-me (Halloumi)"),
     new Joke("What kind of cheese do you use to disguise a small horse", "Mask-a-pony (Mascarpone)")
@@ -23,11 +23,12 @@ export class JokeListComponent implements AfterViewInit, AfterContentInit {
 
   ngAfterViewInit() {
     console.log(`ngAfterViewInit - jokeViewChild is ${this.jokeViewChild}`);
-    // let myjokes: JokeComponent[] = this.jokeViewChildren.toArray();
-    // console.log(jokes)
-  }
-  ngAfterContentInit() {
-    console.log('the insidengContent that actually comes from host component is: ' + this.insideNgContent)
+    this.jokeViewChildren?.forEach(joke => console.log(joke));
+    console.log(`ngAfterViewInit - headerEl is ${this.headerEl}`);
+    if (this.headerEl != undefined) {
+      this.headerEl.nativeElement.textContent = "Best Joke Machine";
+    }
+    
   }
 
   // addJoke() {
