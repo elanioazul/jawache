29d781af746555f4a57bcd60d03539b728a6aaf2
elanioazul_hugo <hugo.herrador@gmail.com>
2021-05-27 17:19:53 +0200

    Add reactive form but still I don t get control over valueChanges method

--- apps/jawache-tutorial/src/app/modules/form/form.module.ts
+++ apps/jawache-tutorial/src/app/modules/form/form.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { FormComponent } from './form/form.component';
 
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {ButtonModule} from 'primeng/button';
 
 
 @NgModule({
   declarations: [
     FormComponent
   ],
+  exports: [FormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    InputTextareaModule,
+    ButtonModule
   ]
 })
 export class myFormModule { }
--- apps/jawache-tutorial/src/app/modules/form/form/form.component.ts
+++ apps/jawache-tutorial/src/app/modules/form/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'jawache-tutorial-form',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  comment = new FormControl("", Validators.required);
+  name = new FormControl("", Validators.required);
+  email = new FormControl("", [ 
+    Validators.required,
+    Validators.pattern("[^ @]*@[^ @]*")
+  ])
+  
+  reactiveform = new FormGroup({
+      "comment": this.comment,
+      "name": this.name,
+      "email": this.email
+    });
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
+    //this.reactiveform.get("comment").valueChanges.subscribe( data => console.log(JSON.stringify(data)))
+  }
+
+  onSubmit() {
+    console.log("Form submitted with this contetn: " + this.reactiveform.value);
   }
 
 }
