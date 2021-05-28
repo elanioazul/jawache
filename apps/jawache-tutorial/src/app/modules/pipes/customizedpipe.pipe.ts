662888aa2578b486b73e8240d6a119a0c4e3151d
elanioazul_hugo <hugo.herrador@gmail.com>
2021-05-28 17:29:26 +0200

    Customized pipe Multiple Parameters

--- apps/jawache-tutorial/src/app/modules/pipes/customizedpipe.pipe.ts
+++ apps/jawache-tutorial/src/app/modules/pipes/customizedpipe.pipe.ts
@@ -5,12 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomizedpipePipe implements PipeTransform {
 
-  transform(value: string, fallback: string): string {
+  transform(value: string, fallback: string, forceHttps: boolean = false): string {
     let image = '';
     if (value) {
       image = value;
     } else {
       image = fallback
+    }
+    if (forceHttps) {
+      if (image.indexOf("https") == -1) {
+        image = image.replace("http", "https");
+      }
     }
     return image;
   }
