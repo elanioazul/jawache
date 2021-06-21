87809a90c42009648d62103be162373b8bd1e7e5
elanioazul_hugo <hugo.herrador@gmail.com>
2021-06-21 12:38:09 +0200

    Create a service and a method in it

--- apps/jawache-tutorial/src/app/modules/httppromises/services/search.service.ts
+++ apps/jawache-tutorial/src/app/modules/httppromises/services/search.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  public apiRoot = 'https://itunes.apple.com/search';
+  public results: any;
+  public loading: boolean;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.results = [];
+    this.loading = false;
+  }
+
+  search(term:string) {
+    const promise = new Promise((resolve, reject) => {
+      const apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
+      this.httpClient.get(apiURL)
+        .toPromise()
+        .then(
+          res => { // Success
+            console.log(res);
+            resolve("done");
+          }
+        );
+    });
+    return promise;
+  }
 }
