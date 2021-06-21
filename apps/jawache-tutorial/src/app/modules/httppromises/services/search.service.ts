fc518511c75075fe67dd048058ed85bbc92d6280
elanioazul_hugo <hugo.herrador@gmail.com>
2021-06-21 17:34:56 +0200

    Point out to http verb what to expect using a interface

--- apps/jawache-tutorial/src/app/modules/httppromises/services/search.service.ts
+++ apps/jawache-tutorial/src/app/modules/httppromises/services/search.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+interface results {
+  resultCount: number,
+  results: any[]
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class SearchService {
   public apiRoot = 'https://itunes.apple.com/search';
-  public results: any;
+  public results: any[];
   public loading: boolean;
 
   constructor(private httpClient: HttpClient) {
@@ -16,12 +22,16 @@ export class SearchService {
   search(term:string) {
     const promise = new Promise((resolve, reject) => {
       const apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
-      this.httpClient.get(apiURL)
+      this.httpClient.get<results>(apiURL)
         .toPromise()
         .then(
           res => { // Success
-            console.log(res);
+            this.results = res['results'];
             resolve("done");
+            console.log(this.results)
+          },
+          msg => { // Error
+          reject(msg);
           }
         );
     });
