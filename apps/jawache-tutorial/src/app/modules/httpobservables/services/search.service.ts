6429ca2fe7fd392dacd1fc425d9460ab92531907
elanioazul_hugo <hugo.herrador@gmail.com>
2021-06-25 17:13:19 +0200

    Creates a method in searchService that returns an observable of serachItems array, instead of a observable of response type like with promises

--- apps/jawache-tutorial/src/app/modules/httpobservables/services/search.service.ts
+++ apps/jawache-tutorial/src/app/modules/httpobservables/services/search.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { Searchitem } from '../clases/searchitem'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  public apiRoot = 'https://itunes.apple.com/search';
+  public results: Searchitem[];
+  public loading: boolean;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.results = [];
+    this.loading = false;
+  }
+
+  search(term:string): Observable<Searchitem[]> {
+    const apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
+    const request = this.httpClient.get<Observable<Searchitem>>(apiURL);
+    return request.pipe(
+      map((res: any) => res.results.map((item:any) => {
+        return new Searchitem(
+          item.trackName,
+          item.artistName,
+          item.trackViewUrl,
+          item.artworkUrl30,
+          item.artistId
+        )
+      }))
+    )
+  }
+
 }
