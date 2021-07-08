3dd26f6e838521f342d954f529961adeeacdfe89
elanioazul_hugo <hugo.herrador@gmail.com>
2021-07-08 14:48:40 +0200

    Define routes in Routes array

--- apps/jawache-tutorial/src/app/app-routing.module.ts
+++ apps/jawache-tutorial/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HomeComponent} from './modules/httppromises/components/home/home.component';
+import { SuperappComponent} from './modules/httppromises/components/superapp/superapp.component';
+
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = []; 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SuperappComponent }
+]; 
 
 @NgModule({
   declarations: [],
