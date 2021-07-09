a1973421e590ed9d537d8efebaee565c93070aa5
elanioazul_hugo <hugo.herrador@gmail.com>
2021-07-09 16:50:02 +0200

    With Parent route parameter passed, I am able in child componets to catch parent params and make more calls to api

--- apps/jawache-tutorial/src/app/modules/httppromises/components/artist-album-list/artist-album-list.component.ts
+++ apps/jawache-tutorial/src/app/modules/httppromises/components/artist-album-list/artist-album-list.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient} from '@angular/common/http';
+interface album {
+  collectionViewUrl: string,
+  discNumber: number;
+  artworkUrl100: string,
+  collectionName: string
+}
 @Component({
   selector: 'jawache-tutorial-artist-album-list',
   templateUrl: './artist-album-list.component.html',
@@ -7,7 +14,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistAlbumListComponent implements OnInit {
 
-  constructor() { }
+  public albums: Array<album> | undefined;
+
+  constructor(
+    private httpClient: HttpClient, 
+    private route: ActivatedRoute,
+  ) { 
+    this.route.parent?.params.subscribe(params => {
+      console.log('params from the parent route is, artistID is' + params)
+      this.httpClient.get<any>(`https://itunes.apple.com/lookup?id=${params["artistId"]}&entity=album`)
+        .toPromise()
+        .then(res => {
+          console.log(res);
+          this.albums = res.results.slice(1);//corto el 1er obj del array que llega que son datos del artista, no de los tracks del artista
+        })
+    })
+  }
 
   ngOnInit(): void {
   }
--- apps/jawache-tutorial/src/app/modules/httppromises/components/artist-track-list/artist-track-list.component.ts
+++ apps/jawache-tutorial/src/app/modules/httppromises/components/artist-track-list/artist-track-list.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient} from '@angular/common/http';
+
+interface track {
+  artworkUrl30: string,
+  trackViewUrl: string,
+  trackName: string,
+  collectionName: string
+}
 @Component({
   selector: 'jawache-tutorial-artist-track-list',
   templateUrl: './artist-track-list.component.html',
@@ -7,12 +15,20 @@ import { Router, ActivatedRoute } from "@angular/router";
 })
 export class ArtistTrackListComponent implements OnInit {
 
+  public tracks: Array<track> | undefined;
+
   constructor(
-    private router: Router, 
-    private route: ActivatedRoute
+    private httpClient: HttpClient, 
+    private route: ActivatedRoute,
   ) {
     this.route.parent?.params.subscribe(params => {
-      console.log(params)
+      console.log('params from the parent route is, artistID is' + params)
+      this.httpClient.get<any>(`https://itunes.apple.com/lookup?id=${params["artistId"]}&entity=song`)
+        .toPromise()
+        .then(res => {
+          console.log(res);
+          this.tracks = res.results.slice(1);//corto el 1er obj del array que llega que son datos del artista, no de los tracks del artista
+        })
     })
    }
 
