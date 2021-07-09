4bdef74c3dee074108d94d1a592fa78ccfa5431d
elanioazul_hugo <hugo.herrador@gmail.com>
2021-07-09 17:44:02 +0200

    Artist comp also catch rout param and make a call to render an async template prop

--- apps/jawache-tutorial/src/app/modules/httppromises/components/artist/artist.component.ts
+++ apps/jawache-tutorial/src/app/modules/httppromises/components/artist/artist.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient} from '@angular/common/http';
 
+interface artist {
+  amgArtistId: number,
+  artistId: number,
+  artistLinkUrl: string,
+  artistName: string,
+  artistType: string,
+  primaryGenreId: number,
+  primaryGenreName: string,
+  wrapperType: string
+}
 @Component({
   selector: 'jawache-tutorial-artist',
   templateUrl: './artist.component.html',
@@ -7,7 +19,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistComponent implements OnInit {
 
-  constructor() { }
+  public selectedArtist: artist | undefined;
+
+  constructor(
+    private httpClient: HttpClient, 
+    private route: ActivatedRoute,
+  ) {
+    this.route.params.subscribe(params => {
+      console.log('This is ArtistComponent: params from the parent route is, artistID is' + params)
+      this.httpClient.get<any>(`https://itunes.apple.com/lookup?id=${params["artistId"]}`)
+        .toPromise()
+        .then(res => {
+          console.log(res);
+          this.selectedArtist = res.results[0];
+        })
+    })
+   }
 
   ngOnInit(): void {
   }
